da3a1d0eee01e46a2579edaefc89fd433a732e11
Jonathan Lorimer <jonathan_lorimer@mac.com>
2024-08-11 21:04:17 -0400

    feat: add pipeline for applicative

--- src/Hasql/Effect.hs
+++ src/Hasql/Effect.hs
@@ -21,7 +21,7 @@ import Control.Monad qualified as M
 import Control.Applicative qualified as A
 import Data.Kind (Type)
 import Hasql.Pool (Pool, use, UsageError)
-import Hasql.Session (Session, SessionError(..))
+import Hasql.Session (Session, SessionError(..), pipeline)
 import Hasql.Session qualified as S
 import Hasql.Statement (Statement)
 import Control.Monad.IO.Class (MonadIO (liftIO))
@@ -30,28 +30,31 @@ import qualified Hasql.Transaction as T
 import qualified Hasql.Transaction.Sessions as T
 import Data.Functor.Contravariant
 import Control.Monad.Free (Free (..), foldFree)
+import qualified Hasql.Pipeline as P
 
 class (forall k. Functor (m k)) => EffA (m :: k -> Type -> Type) where
-  type Empty m :: k
-  type Append m (t :: k) (t' :: k) :: k
+  type EmptyA m :: k
+  type AppendA m (t :: k) (t' :: k) :: k
 
-  pure  :: a -> m (Empty m) a
-  (<*>) :: m t (a -> b) -> m t' a -> m (Append m t t') b
+  pure  :: a -> m (EmptyA m) a
+  (<*>) :: m t (a -> b) -> m t' a -> m (AppendA m t t') b
 
 class EffA m => EffM (m :: k -> Type -> Type) where
-  return :: a -> m (Empty m) a
-  return = pure
+  type EmptyM m :: k
+  type AppendM m (t :: k) (t' :: k) :: k
 
-  (>>=) :: m t a -> (a -> m t' b) -> m (Append m t t') b
+  return :: a -> m (EmptyM m) a
 
-  (>>) :: m t a -> m t' b -> m (Append m t t') b
+  (>>=) :: m t a -> (a -> m t' b) -> m (AppendM m t t') b
+
+  (>>) :: m t a -> m t' b -> m (AppendM m t t') b
   x >> y = x >>= const y
 
 ----
 
 data ManipulationKind = Read | Write
 
-data SessionMode = Command | Transaction
+data SessionMode = Pipeline | Command | Transaction
 
 data HasqlGrade = HasqlGrade ManipulationKind SessionMode
 
@@ -62,25 +65,35 @@ class HasConnections m where
 newtype HasqlEff (k :: HasqlGrade) result = HasqlEff { getFree :: Free (Statement ()) result } 
   deriving newtype (Functor, A.Applicative, M.Monad)
 
+
 instance EffA HasqlEff where
-  type Empty HasqlEff = 'HasqlGrade 'Read 'Command
+  type EmptyA HasqlEff = 'HasqlGrade 'Read 'Pipeline
 
-  -- TODO: Figure these semantics out lol
-  type Append HasqlEff ('HasqlGrade 'Read s)  ('HasqlGrade 'Read s)  = 'HasqlGrade 'Read s
-  type Append HasqlEff ('HasqlGrade 'Write s) ('HasqlGrade _ s)      = 'HasqlGrade 'Write 'Transaction
-  type Append HasqlEff ('HasqlGrade _ s)      ('HasqlGrade 'Write s) = 'HasqlGrade 'Write 'Transaction
+  type AppendA HasqlEff ('HasqlGrade 'Read s)  ('HasqlGrade 'Read s)  = 'HasqlGrade 'Read s
+  type AppendA HasqlEff ('HasqlGrade 'Write s) ('HasqlGrade _ s)      = 'HasqlGrade 'Write s
+  type AppendA HasqlEff ('HasqlGrade _ s)      ('HasqlGrade 'Write s) = 'HasqlGrade 'Write s
 
-  pure :: a -> HasqlEff ('HasqlGrade 'Read 'Command) a
+  pure :: a -> HasqlEff (EmptyA HasqlEff) a
   pure = HasqlEff . Pure
 
-  (<*>) :: HasqlEff (k :: HasqlGrade) (a -> b) -> HasqlEff (k' :: HasqlGrade) a -> HasqlEff (Append HasqlEff k k') b
+  (<*>) :: HasqlEff (k :: HasqlGrade) (a -> b) -> HasqlEff (k' :: HasqlGrade) a -> HasqlEff (AppendA HasqlEff k k') b
   HasqlEff f <*> HasqlEff a = HasqlEff $ f A.<*> a
 
-instance EffM HasqlEff where
-  return :: a -> HasqlEff (Empty HasqlEff) a
-  return = pure
+type family AppendMHasqlEff (a :: HasqlGrade) (b :: HasqlGrade) :: k where
+  AppendMHasqlEff ('HasqlGrade 'Read 'Transaction) ('HasqlGrade 'Read _) = 'HasqlGrade 'Read 'Transaction
+  AppendMHasqlEff ('HasqlGrade 'Read _) ('HasqlGrade 'Read 'Transaction) = 'HasqlGrade 'Read 'Transaction
+  AppendMHasqlEff ('HasqlGrade 'Read _) ('HasqlGrade 'Read _) = 'HasqlGrade 'Read 'Command
+  AppendMHasqlEff ('HasqlGrade _ _) ('HasqlGrade 'Write _) = 'HasqlGrade 'Write 'Transaction
+  AppendMHasqlEff ('HasqlGrade 'Write _) ('HasqlGrade _ _) = 'HasqlGrade 'Write 'Transaction
 
-  (>>=) :: HasqlEff t a -> (a -> HasqlEff t' b) -> HasqlEff (Append HasqlEff t t') b
+instance EffM HasqlEff where
+  type EmptyM HasqlEff = 'HasqlGrade 'Read 'Command
+  type AppendM HasqlEff g1 g2 = AppendMHasqlEff g1 g2
+
+  return :: a -> HasqlEff (EmptyM HasqlEff) a
+  return = HasqlEff . Pure
+
+  (>>=) :: HasqlEff t a -> (a -> HasqlEff t' b) -> HasqlEff (AppendM HasqlEff t t') b
   (HasqlEff a) >>= f = HasqlEff $ a M.>>= (getFree . f)
 
 read
@@ -107,6 +120,9 @@ instance GetSessionMode ('HasqlGrade m 'Command) where
 instance GetSessionMode ('HasqlGrade m 'Transaction) where
   sessionMode _ = Transaction
 
+instance GetSessionMode ('HasqlGrade m 'Pipeline) where
+  sessionMode _ = Pipeline
+
 instance GetManipulationKind ('HasqlGrade 'Read s) where
   manipulationKind _ = Read
 
@@ -114,12 +130,8 @@ instance GetManipulationKind ('HasqlGrade 'Write s) where
   manipulationKind _ = Write
 
 run
-  :: forall k m r. 
-    ( HasConnections m
-    , MonadIO m
-    , GetManipulationKind k
-    , GetSessionMode k
-    )
+  :: forall k m r. ( GetManipulationKind k, GetSessionMode k)
+  => ( HasConnections m, MonadIO m)
   => HasqlEff (k :: HasqlGrade) r
   -> m (Either UsageError r)
 run (HasqlEff e) = do
@@ -130,6 +142,7 @@ run (HasqlEff e) = do
       natTrans = case sessionMode (Proxy @k) of
               Command -> S.statement ()
               Transaction -> T.transaction T.Serializable T.Write . T.statement ()
+              Pipeline -> pipeline . P.statement ()
   liftIO . use c $ foldFree natTrans e
 
 
